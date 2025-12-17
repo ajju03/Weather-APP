643d17fe45ed4db46ce04192e2c3d21a59388dc8
ajju03 <ajayprasanna7@gmail.com>
2025-12-17 13:11:49 +0530

    Port number updated in index.js to 3000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
